7daafcd30e9a222814d07b75852156dc85acb2dd
miyeok2ee <miyeok2ee@gmail.com>
2020-03-14 02:11:44 +0900

    17087 숨바꼭질6 수학 최대공배수 gcd

---
+++ 17087_숨바꼭질6/17087.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    int c;
+    while (b != 0)
+    {
+        c = a % b;
+        a = b;
+        b = c;
+    }
+    return a;
+}
+
+int main() 
+{
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int n, s;
+    int gcd_tmp = -1;
+    cin >> n >> s;
+    while (n--)
+    {
+        int a;
+        cin >> a;
+        int distance = (s > a) ? s-a: a-s;
+        if(gcd_tmp != -1)
+        {
+            gcd_tmp = gcd(gcd_tmp, distance);
+        } 
+        else
+        {
+            gcd_tmp = distance;
+        }
+    }
+    cout << gcd_tmp << '\n';
+
+
+    return 0;
+}
