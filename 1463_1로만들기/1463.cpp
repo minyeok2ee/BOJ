cfff6edb4f5a95844fb4b73caa8a20dcf7a099ea
miyeok2ee <miyeok2ee@gmail.com>
2020-03-26 17:37:48 +0900

    1463 1로만들기
    다이나믹 프로그래밍 동적계획법 dynamic programming

--- 1463_1로만들기/1463.cpp
+++ 1463_1로만들기/1463.cpp
@@ -2,25 +2,24 @@
 #include <vector>
 using namespace std;
 
-
 int main()
 {
     int n;
     cin >> n;
-    vector<int> dp(n+1);
-    
-    dp[1] = 0;
-    for(int i=2; i<=n; i++)
-    {
-        dp[i] = dp[i-1] + 1;
+    vector<int> dp(n + 1);
 
-        if(i %2 == 0 && dp[i] > dp[i/2] + 1)
+    dp[1] = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + 1;
+
+        if (i % 2 == 0 && dp[i] > dp[i / 2] + 1)
         {
-            dp[i] = dp[i/2] + 1;
+            dp[i] = dp[i / 2] + 1;
         }
-        if(i%3 == 0 && dp[i] > dp[i/3] + 1)
+        if (i % 3 == 0 && dp[i] > dp[i / 3] + 1)
         {
-            dp[i] = dp[i/3] + 1;
+            dp[i] = dp[i / 3] + 1;
         }
     }
     cout << dp[n] << '\n';
