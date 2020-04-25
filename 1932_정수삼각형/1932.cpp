59c77c0adff44f2adae8737d14a47d369761588e
miyeok2ee <miyeok2ee@gmail.com>
2020-04-25 23:23:10 +0900

    1932 정수삼각형
    동적계획법 다이나믹프로그래밍 dp dynamicprogramming

--- 1932_정수삼각형/1932.cpp
+++ 1932_정수삼각형/1932.cpp
@@ -27,7 +27,7 @@ int main() {
 				}
 				else		// Áß°£
 				{
-                	dp[i][j] = max(dp[i][j] + dp[i - 1][j], dp[i][j] + dp[i - 1][j - 1]);
+                	dp[i][j] += max(dp[i - 1][j], dp[i - 1][j - 1]);
 				}
 			}
 		}
